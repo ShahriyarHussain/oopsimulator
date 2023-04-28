fea21349ff6a3c1f521f9ea8161349e103766ef0
ShahriyarHussain <shossainshihab36@gmail.com>
2023-04-28 12:30:18 +0600

    Initial Commit

---
+++ src/main/java/com/cse716/oopsimulator/OopsimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.cse716.oopsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopsimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopsimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cse716/oopsimulator/OopsimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cse716.oopsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopsimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
